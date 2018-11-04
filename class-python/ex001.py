3d008b1d7421a93ba24db9d5bcee1898cecdfff9
sh4hids <shahid.bracu@gmail.com>
2018-11-04 12:11:31 +0600

    Sun Nov  4 12:11:23 +06 2018

---
+++ class-python/ex001.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("Hello there!")
+print("I just started learning python.")
+print("It's very fun to do...!")
---
+++ class-python/ex005.py
@@ -0,0 +1,18 @@
+my_name = 'Shahidul Islam'
+my_age = 25
+my_height = 63 # inches
+my_weight = 59 # kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} kg heavy.")
+print(f"Actually that's very heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# a tricky line
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}")
