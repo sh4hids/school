cc5d37d63be4155483fc159d1ab7b156881be1f2
Shahidul Islam Majumder <dev@shahid.pro>
2019-12-31 15:12:59 +0600

    :tada: learnt a lot of things

---
+++ class-python/ex012.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+age = input("How old are you? ")
+
+print(f"You name is {name} and you are {age} years old.")
---
+++ class-python/ex016.py
@@ -0,0 +1,31 @@
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want to press CTRL+C (^C).")
+print("If you want to continue, press Enter.")
+
+input("?")
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I am going to ask you for three lines.")
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write this to the file.")
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it.")
+target.close()
