f870548f3febd2720fa33e8ecc590d04e40c6137
Shahidul Islam Majumder <dev@shahid.pro>
2020-01-15 16:13:40 +0600

    Add new python experience

---
+++ class-python/ex039.py
@@ -0,0 +1,47 @@
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+print('-' * 10)
+
+print("NY State has: ", cities['NY'])
+print("OR State has: ", cities['OR'])
+
+print('-' * 10)
+print("Michigan's abbreviation is: ", states['Michigan'])
+print("Florida's abbreviation is: ", states['Florida'])
+
+print('-' * 10)
+print("Michigan has: ", cities[states['Michigan']])
+print("Florida has: ", cities[states['Florida']])
+
+print('-' * 10)
+for state, abbrev in list(states.items()):
+    print(f"{abbrev} state is abbreviated {abbrev}")
+    print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+
+state = states.get('Texas')
+
+if not state:
+    print("Sorry, no Texas")
+
+city = cities.get('TX', 'Does not exist!')
+print(f"The city for the sate TX is: {city}")
+
+print('-' * 10)
+print(states.items())
