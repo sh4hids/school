3160fd35ce47dcbd393c7884e051b4bd2f05e00a
Shahidul Islam Majumder <dev@shahid.pro>
2020-04-09 09:23:48 +0600

    📦 NEW: Add new golang experience

--- class-golang/go-webservice/main.go
+++ class-golang/go-webservice/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
   "fmt"
+
+  "github.com/sh4hids/school/class-golang/models"
 )
 
 func main() {
-  fmt.Println("Hello world. I am one of the gophers now!!!")
+  u := models.User{
+    ID: 2,
+    FirstName: "John",
+    LastName: "Doe",
+  }
+
+  fmt.Println(u)
 }
 
