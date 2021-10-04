1f15d690935f698794caab8e27e0c98aca446a6b
sh4hids <dev@shahid.pro>
2021-10-04 14:04:01 +0600

    ➕ add lessons from decorators

--- class-typescript/src/decorators_1.ts
+++ class-typescript/src/decorators_1.ts
@@ -1,8 +1,31 @@
-function addFuel<T extends {new (...args: any[]): {}}>(constructor: T) {
+function addFuel<T extends { new (...args: any[]): {} }>(constructor: T) {
   return class extends constructor {
     fuel = 100;
-  }
+  };
 }
+
+const execTime = (
+  target: Object,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) => {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    console.log(`Launching start: ${new Date().getTime()}`);
+    const result = originalMethod.apply(this, args);
+    console.log(`Launching end: ${new Date().getTime()}`);
+    return result;
+  };
+
+  return descriptor;
+};
+
+const changeValue = (value: any) => (target: Object, propertyKey: string) => {
+  console.log(target);
+  console.log(propertyKey);
+  Object.defineProperty(target, propertyKey, { value });
+};
 
 /* Following is a trick to avoid error */
 
@@ -10,20 +33,28 @@ interface WithFuel {
   fuel: string;
 }
 
-interface Rocket extends WithFuel {};
+interface Rocket extends WithFuel {}
 
 /* Trick ends here */
 
 @addFuel
-class Rocket {}
+class Rocket {
+  @changeValue(1)
+  version = 0;
+
+  @execTime
+  launch() {
+    console.log(`Launching in 3... 2... 1... 🚀`);
+  }
+}
 
 const myRocket = new Rocket();
 console.log("Rocket fule:", myRocket.fuel);
+myRocket.launch();
 
 /* Following is another trick to avoid error */
 
-const Bus = addFuel(class {})
+const Bus = addFuel(class {});
 
 const myBus = new Bus();
 console.log("Bus fuel:", myBus.fuel);
-
