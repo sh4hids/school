7115eafe1768e7eff36b0ebd5ee62c501066c4f7
Shahidul Islam Majumder <dev@shahid.pro>
2021-10-23 20:21:20 +0600

    ➕ add lessons about rtk

---
+++ class-typescript/ts-react/src/store.ts
@@ -0,0 +1,50 @@
+import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { type } from "os";
+
+interface Todo {
+  id: number;
+  isDone: boolean;
+  text: string;
+}
+
+interface TodoSliceState {
+  todos: Todo[];
+}
+
+const initialState: TodoSliceState = {
+  todos: [],
+};
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.todos = [
+        ...state.todos,
+        {
+          id: state.todos.length,
+          text: action.payload,
+          isDone: false,
+        },
+      ];
+    },
+    removeTodo: (state, action: PayloadAction<number>) => {
+      state.todos = state.todos.filter(({ id }) => id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todosSlice.actions;
+
+const store = configureStore({
+  reducer: {
+    todos: todosSlice.reducer,
+  },
+});
+
+type RootState = ReturnType<typeof store.getState>;
+
+export const selectTodos = (state: RootState) => state.todos.todos;
+
+export default store;
